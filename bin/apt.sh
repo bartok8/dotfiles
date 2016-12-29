c6e8e1cb3dcf976da5039efbf694f29b0ff63c17
nogu <nogu@nogu-ubuntu-ja-16.04>
2016-12-30 01:40:42 +0900

    update

---
+++ bin/apt.sh
@@ -0,0 +1,104 @@
+#! /bin/bash -x
+
+APT_GET () {
+    a=$1
+    apt-get install $a
+}
+
+APT_INST () {
+    a=$1
+    apt install $a
+}
+
+APT_REMV () {
+    a=$1
+    apt-get remove $a
+}
+
+#apt-get install gnome-panel
+#apt-get install emacs
+
+# APT_GET git
+# APT_GET ruby
+# APT_GET python
+# APT_GET doxygen
+# APT_GET jed
+# APT_GET zsh
+# APT_GET ctags
+# APT_GET emacs-goodies-el
+# APT_GET exuberant-ctags
+# APT_GET android-tools-adb
+# APT_GET fontconfig
+# APT_GET xfonts-intl-japanese
+# APT_GET xfonts-intl-japanese-big
+# APT_GET xfonts-jisx0213
+# APT_GET xfonts-kaname
+# APT_GET xfonts-marumoji
+# APT_GET xfonts-mona
+# APT_GET xfonts-mplus
+# APT_GET fonts-takao-gothic
+# APT_GET fonts-takao-mincho
+# APT_GET fonts-takao-pgothic
+# APT_GET fonts-dejima-mincho
+# APT_GET fonts-hanazono
+# APT_GET fonts-ipaexfont
+# APT_GET fonts-ipaexfont-gothic
+# APT_GET fonts-ipaexfont-mincho
+# APT_GET fonts-ipafont
+# APT_GET fonts-ipafont-gothic
+# APT_GET fonts-ipafont-mincho
+# APT_GET fonts-ipamj-mincho
+# APT_GET fonts-kiloji
+# APT_GET fonts-kouzan-mouhitsu
+# APT_GET fonts-migmix
+# APT_GET fonts-misaki
+# APT_GET fonts-mona
+# APT_GET fonts-monapo
+# APT_GET fonts-motoya-l-cedar
+# APT_GET fonts-motoya-l-maruberi
+# APT_GET fonts-mplus
+# APT_GET fonts-sawarabi-gothic
+# APT_GET fonts-sawarabi-mincho
+# APT_GET fonts-seto
+# APT_GET fonts-takao
+# APT_GET fonts-umeplus
+# APT_GET fonts-vlgothic
+# APT_GET xfonts-ayu
+# APT_GET xfonts-shinonome
+# APT_GET fonts-ipafont-nonfree-jisx0208
+# APT_GET fonts-ipafont-nonfree-uigothic
+# APT_GET xfonts-naga10
+# APT_GET yamllint
+# APT_GET libwww-perl
+# APT_GET jd
+# APT_GET emacs-mozc
+# APT_GET emacs-mozc-bin
+# APT_GET ibus-mozc
+# APT_GET mozc-serer
+# APT_GET mozc-utils-gui
+# APT_GET w3m-el
+APT_GET migemo-el
+
+
+
+
+
+# vivaldi & flush
+# APT_GET libappindicator1
+# APT_GET libindicator7
+# APT_GET adobe-flashplugin # ng
+# APT_INST adobe-flashplugin
+# APT_INST ./arc/ubuntu-restricted-extras_59_amd64.deb
+# APT_GET flashplugin-installer
+# APT_REMV flashplugin-installer #1
+# APT_REMV vivaldi-stable #2
+# APT_GET browser-plugin-freshplayer-pepperflash #3
+# APT_INST ./arc/vivaldi-stable_1.5.658.44-1_amd64.deb #4
+
+#APT_GET ubuntu-restricted-addons #ng
+#APT_GET ubuntu-restricted-extras #ng
+
+
+
+# ends here.
+
