58caa392c14c5bc0ab6e9129d1e4d23a9eb36fa7
bartok8 <bartok8@gmail.com>
2020-11-18 03:26:22 +0900

    update kuji

--- py/lucky.py
+++ py/lucky.py
@@ -62,6 +62,7 @@ lstURL = [
     "https://kuji.rakuten.co.jp/69334ccff1", # syaken
     "https://kuji.rakuten.co.jp/d9135bc12d", # toto
     "https://kuji.rakuten.co.jp/643377c7a5?scid=su_8289", # deliverly
+    "https://kuji.rakuten.co.jp/bec377e2d5", # deliverly?
     "https://kuji.rakuten.co.jp/9ea32a8dfa", # calendar
     #"https://kuji.rakuten.co.jp/1b628755e1", # edy
     "https://kuji.rakuten.co.jp/7393386d27", # pay
@@ -70,6 +71,32 @@ lstURL = [
     "https://kuji.rakuten.co.jp/42136c5d7d", # books
     #"https://www.infoseek.co.jp/Luckylot", # infoseek
     "https://kuji.rakuten.co.jp/6432148795", # recipi sp
+
+    # additional...
+    "https://point.rakuten.co.jp/doc/lottery/lucky/", #pointclub appli lucky kuji
+    "https://kuji.rakuten.co.jp/c2d372fbbe", # smaho sakana
+    "https://kuji.rakuten.co.jp/490372ef94", # smaho sakana
+    "https://kuji.rakuten.co.jp/68b371ed7d", # koko hore
+    "https://kuji.rakuten.co.jp/b9436b1ddd", # gurunavi sp
+    "https://kuji.rakuten.co.jp/2a836b2e57", # gurunavi sp
+    "https://kuji.rakuten.co.jp/ffa36a07d3", # beauty
+    "https://kuji.rakuten.co.jp/34d356d5d5", # reward
+    "https://kuji.rakuten.co.jp/a24364f880", # point club
+    "https://kuji.rakuten.co.jp/6233615e79", # omikuji miko
+    "https://kuji.rakuten.co.jp/4ad36145c2", # lucky kuji
+    "https://kuji.rakuten.co.jp/69334ccff1", # syaken
+    "https://kuji.rakuten.co.jp/1473379bb1", # point club
+    "https://kuji.rakuten.co.jp/bb63377557", # energy
+    "https://kuji.rakuten.co.jp/eff3376c7a", # energy
+    "https://kuji.rakuten.co.jp/7a1321943c", # lucky kuji
+    "https://kuji.rakuten.co.jp/a29321bc36", # lucky kuji
+    "https://kuji.rakuten.co.jp/ad1321af05", # lucky kuji
+    "https://kuji.rakuten.co.jp/34e2cb79fa", # calendar
+    "https://kuji.rakuten.co.jp/b312abddc5", # 
+    "https://kuji.rakuten.co.jp/8212abcffe", # syousya gentei
+    "https://kuji.rakuten.co.jp/4592a41e4c", # smaho lucky
+    "https://kuji.rakuten.co.jp/d141c5441b", # books
+    "https://kuji.rakuten.co.jp/ffc1c52299", # books
 
     "https://kuji.rakuten.co.jp/b3c3746f95", # 2020/11/4 marathon   
     
